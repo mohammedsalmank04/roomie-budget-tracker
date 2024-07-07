6f13d6575328e3cef348f4326f8d4a8d1df27ebe
mohammedsalmank04 <mohammedsalmank04@gmail.com>
2024-07-06 22:35:20 -0400

    initialCommit

---
+++ src/main/java/com/salman/roomieBudgetTracker/RoomieBudgetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.salman.roomieBudgetTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomieBudgetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomieBudgetTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salman/roomieBudgetTracker/RoomieBudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salman.roomieBudgetTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieBudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
